8331549aeef1d34a8e4b0b9855904a73b02ce998
Chirashree18 <chirashreemahato18@gmail.com>
2025-07-27 17:37:52 +0530

    Update age validation

--- conditionals/index.js
+++ conditionals/index.js
@@ -1,6 +1,25 @@
-let age = 20;
-if ( age > 18 ) {
+let age = 17;
+let grace = 2;
+age += grace;
+console.log(age);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+console.log(age % grace);
+if ( age + grace > 18 ) {
     console.log("You can drive. ");
 } else {
     console.log("You can not drive. ")
 }
+a = 6
+b = 8
+let c = a>b ? (a - b) : (b- a);
+/* 
+translate to :
+if (a>b) {
+    let c = a-b;
+} else {
+ let c =  b-a; 
+}
+*/
